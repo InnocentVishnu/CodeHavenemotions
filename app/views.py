f349797c6e03cb807f802c958167c67b4c6d91c4
InnocentVishnu <111436468+InnocentVishnu@users.noreply.github.com>
2025-01-18 10:49:52 +0530

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse, JsonResponse
+from .camera import VideoCamera
+import json
+
+try:
+    camera = VideoCamera()  
+except Exception as e:
+    print(e)
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@gzip.gzip_page
+def livefe(request):
+    try:
+        return StreamingHttpResponse(gen(camera), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  
+        pass
+
+def index(request, *args, **kwargs):
+    return render(request, 'index.html')
+
+def get_emotions(request):
+    emotions = camera.get_emotions() 
+    return JsonResponse(emotions)
